4d88c970f8af658c4dfe972cb36b73fe4ef8e533
maoxinhaizhi <maoxinhaizhi@163.com>
2017-08-31 01:06:51 +0800

    【提交时间】:2017-08-31 【提交人员】:高祥 【提交原因】:解决冲突

---
+++ src/main/java/net/shopxx/service/FiBankbookBalanceService.java
@@ -0,0 +1,63 @@
+package net.shopxx.service;
+
+import java.util.List;
+
+import net.shopxx.Filter;
+import net.shopxx.Order;
+import java.math.BigDecimal;
+
+import net.shopxx.Page;
+import net.shopxx.Pageable;
+import net.shopxx.entity.Country;
+import net.shopxx.entity.FiBankbookBalance;
+import net.shopxx.entity.Member;
+
+/**
+ * Service - 存折
+ * 
+ * @author gaoxiang
+ * @version 5.0.3
+ */
+public interface FiBankbookBalanceService extends BaseService<FiBankbookBalance, Long> {
+	/**
+	 * 根据会员编号查找存折账号
+	 * @param userCode
+	 * @return
+	 */
+	List<FiBankbookBalance> findList(Member member, Integer count, List<Filter> filters, List<Order> orders);
+	/**
+	 * 根据会员编号查找存折账号
+	 * @param userCode
+	 * 
+	 */
+	FiBankbookBalance findByKey(String fiBanKey, String fiBanValue);
+	
+	/**
+	 * 查找实体对象分页
+	 *
+	 * @param member
+	 *            会员
+	 * @param country
+	 *            国家
+	 * @param pageable
+	 *            分页信息
+	 * @return 实体对象分页
+	 */
+	Page<FiBankbookBalance> findPage(Member member, Country country, Pageable pageable);
+	
+	/**
+	 * 根据usercode和type获取FiBankbookBalance
+	 * @param usercode
+	 * @param type
+	 * @return
+	 */
+	FiBankbookBalance find(Member member, FiBankbookBalance.Type type);
+	
+	/**
+	 * 余额更新
+	 * @param fiBankbookBalance
+	 * @param amount
+	 * @throws Exception 
+	 */
+	void addBalance(FiBankbookBalance fiBankbookBalance, BigDecimal amount) throws Exception;
+}
